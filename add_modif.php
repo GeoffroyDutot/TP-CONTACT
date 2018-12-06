7c89b981baac48bd9d1c7045bd708dad9f2d8d75
simonbourrasseau <simon.bourrasseau@imie.fr>
2018-12-06 15:16:54 +0100

    rajout SQL

--- add_modif.php
+++ add_modif.php
@@ -1,23 +1,36 @@
 <?php
 
-$host = '172.0.0.1';
+$host = 'lacalhost';
 $port = 3306;
 $database = 'annuaire';
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database",'root','');
 
-class contact {
-	private $prenom; 
-	private $nom;
-	private $tel;
-	private $email; 
-	private $id;
 
-	public function __construct(string $pPrenom, string $pNom, int $pTel, $pEmail, int $pId) {
-		$this->Prenon = $pPrenom;
-		$this->Nom = $pNom;
-		$this->Tel = $pTel;
-		$this->Email = $pEmail;
-		$this->Id = $pId;
+class Contact {
+	private $contact_prenom; 
+	private $contact_nom;
+	private $contact_tel;
+	private $contact_email; 
+	private $contact_id;
+
+	public function __construct(string $pContact_Prenom, string $pContact_Nom, int $pContact_Tel, string $pContact_Email, int $pContact_Id) {
+		$this->contact_prenom = $pContact_Prenom;
+		$this->contact_nom = $pContact_Nom;
+		$this->contact_tel = $pContact_Tel;
+		$this->contact_email = $pContact_Email;
+		$this->contact_id = $pContact_Id;
 	}
 }
+
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database",'root','');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
+//    var_dump("Bad credentials");
+} finally {
+    $pdo = null;
+}  
 
