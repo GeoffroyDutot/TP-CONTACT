19d25595b79a832b46a1afa3f1c9f6ea35dfa991
NathanLibouton <libouton.nathan@gmail.com>
2018-12-06 15:30:29 +0100

    Modification dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<table>
+	<thead>
+		<tr>
+			<th>id</th>
+			<th>Nom</th>
+			<th>Action</th>
+		</tr>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>Jean pierre</td>
+				<td><form method="POST" action="">
+					<input type="submit" value="Modifier" name="update">
+					<input type="submit" value="Supprimer" name="delete">
+				</form></td>
+			</tr>
+		</tbody>
+	</thead>
+</table>
