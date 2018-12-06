93057e23aaeaa3fa2b7a8a71d3367ab7810a1df5
simonbourrasseau <simon.bourrasseau@imie.fr>
2018-12-06 14:36:12 +0100

    ajout database

--- add_modif.php
+++ add_modif.php
@@ -2,20 +2,22 @@
 
 $host = '172.0.0.1';
 $port = 3306;
-$database = 'test';
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database",'','');
+$database = 'annuaire';
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database",'root','');
 
 class contact {
 	private $prenom; 
 	private $nom;
 	private $tel;
 	private $email; 
+	private $id;
 
-	public function __construct(string $pPrenom, string $pNom, int $pTel, $pEmail) {
+	public function __construct(string $pPrenom, string $pNom, int $pTel, $pEmail, int $pId) {
 		$this->Prenon = $pPrenom;
 		$this->Nom = $pNom;
 		$this->Tel = $pTel;
 		$this->Email = $pEmail;
+		$this->Id = $pId;
 	}
 }
 
