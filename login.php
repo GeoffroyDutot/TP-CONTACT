4d9ea76dab236b3ca8a24c2aa8266c52adfa172d
ValentinCloarec <valentin.cloarec@imie.fr>
2018-12-06 15:28:39 +0100

    geoffroy le dev

--- login.php
+++ login.php
@@ -47,14 +47,14 @@
                            }
                         }
                     ?>
-                    
+
     <form class="form-signin">
       <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="email" class="sr-only">Votre email :</label>
-      <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
-      <label for="password" class="sr-only">Password</label>
-      <input type="password" id="password" class="form-control" placeholder="Password" required>
+      <input type="email" id="email" class="form-control" placeholder="Adresse email" required autofocus>
+      <label for="password" class="sr-only">Mot de passe :</label>
+      <input type="password" id="password" class="form-control" placeholder="Mot de passe" required>
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
