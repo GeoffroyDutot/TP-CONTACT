2ed63a7e8b45431c6dfe4a923622111112b538bf
simonbourrasseau <simon.bourrasseau@imie.fr>
2018-12-07 14:05:33 +0100

    ajout boutton

--- nouveau.php
+++ nouveau.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: simon
- * Date: 07/12/2018
- * Time: 12:16
- */
+
 
 ?><DOCTYPE html>
 <html lang="fr">
@@ -16,28 +11,27 @@
 
 <h1>ajout ou modifidation de contact</h1>
 
-<form>
+<form type="POST" action="ajout.php">
 <table>
     <tr>
         <td>Nom :</td>
-        <td><input type="text"></td>
+        <td><input type="text" name="contact_nom"></td>
     </tr>
     <tr>
         <td>Prenom : </td>
-        <td><input tupe="text"></td>
+        <td><input tupe="text" name="contact_prenom"></td>
     </tr>
     <tr>
         <td>Telephone : </td>
-        <td><input type="text"></td>
+        <td><input type="text" name="contact_tel"></td>
     </tr>
     <tr>
         <td>Email :</td>
-        <td><input type="text"></td>
+        <td><input type="email" name="contact_email"></td>
     </tr>
 </table>
 
-
-    <input type ="button" value="VALIDER">
+    <input type ="submit" value="VALIDER">
 
 </form>
 
