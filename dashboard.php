88281cc73ebf8ef660a03e933721190386386c9d
GeoffroyDutot <dutot.geoffroyplaposte.net>
2018-12-07 14:13:30 +0100

    Tableau de contact

--- dashboard.php
+++ dashboard.php
@@ -22,7 +22,6 @@ catch (Exception $e)
 	</head>
 	<body>
 
-<<<<<<< HEAD
 <table class="table">
   <thead class="thead-light">
     <tr>
@@ -53,35 +52,6 @@ catch (Exception $e)
     </tr>
   </tbody>
 </table>
-=======
-
-		  <?php
-      if(isset($_POST['Ajouter'])) {
-
-      header("Location: nouveau.php");
-
-       }
-   
-      ?>
-		
-
-		<table>
-
-		<thead>
-		<tr>
-			<th>id</th>
-			<th>Nom</th>
-			<th>Action</th>
-		</tr>
-		</thead>
-		<tbody>
-
-
-
-			<tr>
-				<td>1</td>
-				<td>Jean pierre</td>
->>>>>>> 2ed63a7e8b45431c6dfe4a923622111112b538bf
 				<td><a href=""><button type="button" class="btn btn-secondary btn-sm">Modifier le contact</button></a>
 				<a href="suppr.php"><button type="button" class="btn btn-secondary btn-sm">Supprimer le contact</button></a>
 				</td>
@@ -92,6 +62,5 @@ catch (Exception $e)
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
 	</body>
 </html>
