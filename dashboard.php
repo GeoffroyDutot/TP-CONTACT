dd52d71185bf783ff7278f8a6abeab834f3f5956
ValentinCloarec <valentin.cloarec@imie.fr>
2018-12-07 14:02:34 +0100

    Ajouter

--- dashboard.php
+++ dashboard.php
@@ -21,10 +21,20 @@ catch (Exception $e)
 		<title>Dashboard</title>
 	</head>
 	<body>
+
+
+		  <?php
+      if(isset($_POST['Ajouter'])) {
+
+      header("Location: dashboard.php");
+
+       }
+   
+      ?>
 		
-		<a href="logout.php"><button type="button" class="btn btn-secondary btn-sm">Deconnexion</button></a>
 
 		<table>
+
 		<thead>
 		<tr>
 			<th>id</th>
@@ -33,6 +43,9 @@ catch (Exception $e)
 		</tr>
 		</thead>
 		<tbody>
+
+
+
 			<tr>
 				<td>1</td>
 				<td>Jean pierre</td>
@@ -41,7 +54,13 @@ catch (Exception $e)
 				</td>
 			</tr>
 		</tbody>
+
 	</table>
+<br>
+	 <input type="submit" value="Ajouter" name="Ajouter"><br><br><br>
+
+<a href="logout.php"><button type="button" class="btn btn-secondary btn-sm">Deconnexion</button></a>
+
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
