65087a08cc440ab82ce2db710a9bed0a77ff0694
GeoffroyDutot <dutot.geoffroyplaposte.net>
2018-12-07 14:09:40 +0100

    Tableau de contact

--- dashboard.php
+++ dashboard.php
@@ -9,8 +9,8 @@ catch (Exception $e)
 {
         die('Erreur : ' . $e->getMessage());
 }
- 
-?> 
+
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -22,45 +22,43 @@ catch (Exception $e)
 	</head>
 	<body>
 
-
-		  <?php
-      if(isset($_POST['Ajouter'])) {
-
-      header("Location: dashboard.php");
-
-       }
-   
-      ?>
-		
-
-		<table>
-
-		<thead>
-		<tr>
-			<th>id</th>
-			<th>Nom</th>
-			<th>Action</th>
-		</tr>
-		</thead>
-		<tbody>
-
-
-
-			<tr>
-				<td>1</td>
-				<td>Jean pierre</td>
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 				<td><a href=""><button type="button" class="btn btn-secondary btn-sm">Modifier le contact</button></a>
 				<a href="suppr.php"><button type="button" class="btn btn-secondary btn-sm">Supprimer le contact</button></a>
 				</td>
 			</tr>
 		</tbody>
-
 	</table>
-<br>
-	 <input type="submit" value="Ajouter" name="Ajouter"><br><br><br>
-
-<a href="logout.php"><button type="button" class="btn btn-secondary btn-sm">Deconnexion</button></a>
-
+  	<a href="logout.php"><button type="button" class="btn btn-secondary btn-sm">Deconnexion</button></a>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
