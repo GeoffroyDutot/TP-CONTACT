54f4727a2adc16458b1b568f64ad7b0543228977
unknown <whyvarn@gmail.com>
2018-12-07 15:41:16 +0100

    Modif d'entrées fonctionnelle

--- edit.php
+++ edit.php
@@ -1,10 +1,16 @@
 <?php
 
-include('config/bdd.php');
-
-
-
-?><DOCTYPE html>
+session_start();
+try
+{
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=annuaire;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+?>
+<DOCTYPE html>
     <html lang="fr">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -18,29 +24,40 @@ include('config/bdd.php');
 
     <h1>modification de contact</h1>
 
+    <?php
+    $contact_id = $_POST['contact_id'];
+    $reqcontact = $bdd->query("SELECT * FROM contact WHERE contact_id = $contact_id");
+
+    while ($recup = $reqcontact->fetch()){
+    ?>
     <form method="post" action="tr_edit.php">
         <table>
+        <tr>
+                <td>ID :</td>
+                <td><input type="text" name="contact_id" value="<?php echo $recup['contact_id']?>" required></td>
+            </tr>
             <tr>
                 <td>Nom :</td>
-                <td><input type="text" name="contact_nom" required></td>
+                <td><input type="text" name="contact_nom" value="<?php echo $recup['contact_nom']?>" required></td>
             </tr>
             <tr>
                 <td>Prenom : </td>
-                <td><input type="text" name="contact_prenom" required></td>
+                <td><input type="text" name="contact_prenom" value="<?php echo $recup['contact_prenom']?>" required></td>
             </tr>
             <tr>
                 <td>Telephone : </td>
-                <td><input type="text" name="contact_tel"></td>
+                <td><input type="text" name="contact_tel" value="<?php echo $recup['contact_tel']?>"></td>
             </tr>
             <tr>
                 <td>Email :</td>
-                <td><input type="email" name="contact_email"></td>
+                <td><input type="email" name="contact_email" value="<?php echo $recup['contact_email']?>"></td>
             </tr>
         </table>
 
         <input type ="submit" value="VALIDER">
 
     </form>
+</body>
+</html>
 
-    </body>
-    </html>
+    <?php } ?>
--- tr_edit.php
+++ tr_edit.php
@@ -3,12 +3,14 @@
 
 //Définition de l'encodage de la page
 header('Content-Type: text/html; charset=utf-8');
+header('Location: dashboard.php');
 
 //On accède à la base de données
 include('config/bdd.php');
 
 
 //Récupération des valeurs
+$contact_id = $_POST['contact_id'];
 $contact_nom = $_POST['contact_nom'];
 $contact_prenom = $_POST['contact_prenom'];
 $contact_tel = $_POST['contact_tel'];
@@ -26,7 +28,7 @@ $contact_email = $_POST['contact_email'];
 
 
 //Préparation de l'envoi des données vers la base de données
-    $req = $bdd->prepare("UPDATE INTO contact(contact_nom, contact_prenom, contact_tel, contact_email) VALUES('$contact_nom', '$contact_prenom', '$contact_tel', '$contact_email')");
+$req = $bdd->prepare("UPDATE contact SET contact_nom='$contact_nom', contact_prenom ='$contact_prenom', contact_tel='$contact_tel', contact_email='$contact_email' WHERE contact_id='$contact_id'");
 
 //Exécution de la requête
     $req->execute(array(
