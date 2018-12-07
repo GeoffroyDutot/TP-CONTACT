7047a3009194c43f3d25d8fbb7a3e400101e68cb
unknown <whyvarn@gmail.com>
2018-12-07 13:50:41 +0100

    Corrections mineures

--- ajout.php
+++ ajout.php
@@ -14,11 +14,11 @@ $contact_prenom = $_POST['contact_prenom'];
 $contact_tel = $_POST['contact_tel'];
 $contact_email = $_POST['contact_email'];
 
-// On vérifie si la variable $contact_id n'est pas vide
+//On vérifie si la variable $contact_id n'est pas vide
 $test = empty($contact_id);
 if($test == false) {
 
-// Connexion à la base de données
+//Connexion à la base de données
     try
     {
         $bdd->exec('SET NAMES utf8');
@@ -29,21 +29,21 @@ if($test == false) {
     }
 
 
-// Préparation de l'envoi des données vers la base de données
+//Préparation de l'envoi des données vers la base de données
     $req = $bdd->prepare("INSERT INTO contact(contact_nom, contact_prenom, contact_tel, contact_email) VALUES(:contact_nom, :contact_prenom, :contact_tel, :contact_email)");
 
-// Exécution de la requête
+//Exécution de la requête
     $req->execute(array(
         'contact_nom' => $_POST['contact_nom'],
         'contact_prenom' => $_POST['contact_prenom'],
         'contact_tel' => $_POST['contact_tel'],
-        'contact_email' => $_POST['contact_email'],
+        'contact_email' => $_POST['contact_email']
     ));
 
 
 }
 
-// Si la variable $contact_id est vide, on renvoit un message
+//Si la variable $contact_id est vide, on renvoit un message
 else{
     echo "Erreur";
 }
